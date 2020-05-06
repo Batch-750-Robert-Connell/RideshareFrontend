f2a97ad0eaf77781c7fd6fa4fccb372bc24b92e6
Michael Starzyk <starzykmichael@yahoo.com>
2020-05-06 15:41:17 -0400

    fixed CORS in car selection
    eliminated an instance of CORS

--- e2e/test/register.spec.js
+++ e2e/test/register.spec.js
@@ -2,17 +2,17 @@
 describe('Sign Up tests', function(){
     let elSignUpLink = element(by.xpath('//*[@id="login-form"]/p/a/signupmodal/a'));
     let elRegisterHeader = element(by.xpath('/html/body/modal-container/div/div/div[1]/h4'));
-    let firstNameInput
-    let lastNameInput
-    let usernameInput
-    let emailInput
-    let phoneInput
-    let addressInput
-    let cityInput
-    let stateInput
-    let zipCodeInput
-    let driverRadio
-    
+    // let firstNameInput
+    // let lastNameInput
+    // let usernameInput
+    // let emailInput
+    // let phoneInput
+    // let addressInput
+    // let cityInput
+    // let stateInput
+    // let zipCodeInput
+    // let driverRadio
+
     it('Sign up link on login page opens Sign up modal', function() {
         browser.get('http://http://54.174.82.153:4200/login');
         elSignUpLink.click();
