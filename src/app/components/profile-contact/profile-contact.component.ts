90d78c57b735e776ccb96f3eb69367fb97b13034
Brian V <34783078+brianvogelsang@users.noreply.github.com>
2020-05-13 14:17:22 -0500

    Update profile-contact.component.ts

--- src/app/components/profile-contact/profile-contact.component.ts
+++ src/app/components/profile-contact/profile-contact.component.ts
@@ -8,73 +8,70 @@ declare var Cleave;
 @Component({
   selector: 'app-profile-contact',
   templateUrl: './profile-contact.component.html',
-  styleUrls: ['./profile-contact.component.scss']
+  styleUrls: ['./profile-contact.component.scss'],
 })
 /**
  * ProfileContactComponent works with the contact information of each user.
  */
 export class ProfileContactComponent implements OnInit {
   profilForm: FormGroup;
-  user : User;
+  user: User;
 
-  constructor(private router: Router, private userService: UserService,private formBuilder: FormBuilder,private snackBar: MatSnackBar) {
-    this.userService.getUserById2(sessionStorage.getItem("userid")).subscribe((response)=>{
-      this.user = response;
-      /**
-       * profilForm is what the user uses to enter thier information while updating their profile.
-       * It uses validation to make sure all fields are filled by the user.
-       */
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {
+    this.userService
+      .getUserById2(sessionStorage.getItem('userid'))
+      .subscribe((response) => {
+        this.user = response;
+        /**
+         * profilForm is what the user uses to enter thier information while updating their profile.
+         * It uses validation to make sure all fields are filled by the user.
+         */
 
-      this.profilForm = this.formBuilder.group({
-        firstName: [this.user.firstName, Validators.required],
-        lastName: [this.user.lastName, Validators.required],
-        email: [this.user.email, Validators.email],
-        phone: [this.user.phoneNumber, Validators.pattern(/^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/)]
+        this.profilForm = this.formBuilder.group({
+          firstName: [this.user.firstName, Validators.required],
+          lastName: [this.user.lastName, Validators.required],
+          email: [this.user.email, Validators.email],
+          phone: [
+            this.user.phoneNumber,
+            Validators.pattern(/^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/),
+          ],
+        });
       });
-
-    });
-
-
-
   }
 
-
-  ngOnInit(){
+  ngOnInit() {
     setTimeout(() => {
       //phone format when user enters their phone number.
       new Cleave('#phone', {
         phone: true,
         phoneRegionCode: 'us',
-        delimiter: '-'
-    });
+        delimiter: '-',
+      });
     }, 1000);
   }
   /**
    * updatesContactInfo is used to update the information of each contact. It uses
    * validation to make sure that all the fields are required.
    */
-  updatesContactInfo(){
-    console.log("ok");
-    console.log(this.profilForm);
+  updatesContactInfo() {
     if (this.profilForm.valid) {
-      console.log(this.user);
-      console.log(this.profilForm.value);
       this.userService.updateUserInfo(this.user);
-      this.snackBar.open("success", "", {
+      this.snackBar.open('success', '', {
         duration: 2000,
         verticalPosition: 'top',
-        panelClass: ['success']
+        panelClass: ['success'],
       });
     } else {
-      console.log("nope");
-      this.snackBar.open("failure", "", {
+      this.snackBar.open('failure', '', {
         duration: 2000,
         verticalPosition: 'top',
-        panelClass: ['failure']
+        panelClass: ['failure'],
       });
     }
-
   }
-
-
 }
