65044da5786cb8f9de19e935fb78fb70828e1137
Philip <schupp@protonmail.com>
2020-05-13 20:13:09 -0400

    created the rider dashboard / table

--- src/app/components/rider-dashboard/rider-dashboard.component.ts
+++ src/app/components/rider-dashboard/rider-dashboard.component.ts
@@ -1,43 +1,212 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/services/user-service/user.service';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { environment } from '../../../environments/environment';
+import { ReservationService } from '../../services/reservation-service/reservation.service';
+import { Reservation } from '../../models/reservation';
+import { CarService } from '../../services/car-service/car.service';
+import { Car } from '../../models/car';
 
 @Component({
   selector: 'app-rider-dashboard',
   templateUrl: './rider-dashboard.component.html',
-  styleUrls: ['./rider-dashboard.component.css']
+  styleUrls: ['./rider-dashboard.component.css'],
 })
 export class RiderDashboardComponent implements OnInit {
+  riderRequest: Reservation[] = [];
+  driverCar: Car;
+  CSS_COLOR_NAMES: Array<string> = [
+    'AliceBlue',
+    'AntiqueWhite',
+    'Aqua',
+    'Aquamarine',
+    'Azure',
+    'Beige',
+    'Bisque',
+    'Black',
+    'BlanchedAlmond',
+    'Blue',
+    'BlueViolet',
+    'Brown',
+    'BurlyWood',
+    'CadetBlue',
+    'Chartreuse',
+    'Chocolate',
+    'Coral',
+    'CornflowerBlue',
+    'Cornsilk',
+    'Crimson',
+    'Cyan',
+    'DarkBlue',
+    'DarkCyan',
+    'DarkGoldenRod',
+    'DarkGray',
+    'DarkGrey',
+    'DarkGreen',
+    'DarkKhaki',
+    'DarkMagenta',
+    'DarkOliveGreen',
+    'DarkOrange',
+    'DarkOrchid',
+    'DarkRed',
+    'DarkSalmon',
+    'DarkSeaGreen',
+    'DarkSlateBlue',
+    'DarkSlateGray',
+    'DarkSlateGrey',
+    'DarkTurquoise',
+    'DarkViolet',
+    'DeepPink',
+    'DeepSkyBlue',
+    'DimGray',
+    'DimGrey',
+    'DodgerBlue',
+    'FireBrick',
+    'FloralWhite',
+    'ForestGreen',
+    'Fuchsia',
+    'Gainsboro',
+    'GhostWhite',
+    'Gold',
+    'GoldenRod',
+    'Gray',
+    'Grey',
+    'Green',
+    'GreenYellow',
+    'HoneyDew',
+    'HotPink',
+    'IndianRed',
+    'Indigo',
+    'Ivory',
+    'Khaki',
+    'Lavender',
+    'LavenderBlush',
+    'LawnGreen',
+    'LemonChiffon',
+    'LightBlue',
+    'LightCoral',
+    'LightCyan',
+    'LightGoldenRodYellow',
+    'LightGray',
+    'LightGrey',
+    'LightGreen',
+    'LightPink',
+    'LightSalmon',
+    'LightSeaGreen',
+    'LightSkyBlue',
+    'LightSlateGray',
+    'LightSlateGrey',
+    'LightSteelBlue',
+    'LightYellow',
+    'Lime',
+    'LimeGreen',
+    'Linen',
+    'Magenta',
+    'Maroon',
+    'MediumAquaMarine',
+    'MediumBlue',
+    'MediumOrchid',
+    'MediumPurple',
+    'MediumSeaGreen',
+    'MediumSlateBlue',
+    'MediumSpringGreen',
+    'MediumTurquoise',
+    'MediumVioletRed',
+    'MidnightBlue',
+    'MintCream',
+    'MistyRose',
+    'Moccasin',
+    'NavajoWhite',
+    'Navy',
+    'OldLace',
+    'Olive',
+    'OliveDrab',
+    'Orange',
+    'OrangeRed',
+    'Orchid',
+    'PaleGoldenRod',
+    'PaleGreen',
+    'PaleTurquoise',
+    'PaleVioletRed',
+    'PapayaWhip',
+    'PeachPuff',
+    'Peru',
+    'Pink',
+    'Plum',
+    'PowderBlue',
+    'Purple',
+    'RebeccaPurple',
+    'Red',
+    'RosyBrown',
+    'RoyalBlue',
+    'SaddleBrown',
+    'Salmon',
+    'SandyBrown',
+    'SeaGreen',
+    'SeaShell',
+    'Sienna',
+    'Silver',
+    'SkyBlue',
+    'SlateBlue',
+    'SlateGray',
+    'SlateGrey',
+    'Snow',
+    'SpringGreen',
+    'SteelBlue',
+    'Tan',
+    'Teal',
+    'Thistle',
+    'Tomato',
+    'Turquoise',
+    'Violet',
+    'Wheat',
+    'White',
+    'WhiteSmoke',
+    'Yellow',
+    'YellowGreen',
+  ];
+  color: String = 'orange';
 
-  /**
-   * Set up the url string to the env var
-   * Creates a new user object
-   */
-  url: string = environment.userUri;
-  currentUser: string = '';
-  requestedDriversId: string = '';
-  requestedDriversName: string = '';
-  requestedDriversCar: string = '';
-  driverId: string = sessionStorage.getItem('driverid');
+  Modal: boolean = false;
 
   constructor(
-    private http: HttpClient,
-    private router: Router,
-  ) { }
-
-
-
+    private carService: CarService,
+    private reservationService: ReservationService
+  ) {}
   ngOnInit() {
+    this.getReservations();
+  }
 
-    if(sessionStorage.getItem('userid') != null){
-      this.currentUser = sessionStorage.getItem('name');
-      this.requestedDriversId = sessionStorage.getItem('driverId');
-      this.requestedDriversName = sessionStorage.getItem('driverName');
-      this.requestedDriversCar = sessionStorage.getItem('driversCar');
-    } else {
-      this.currentUser = '';
-     }
-    }
+  getReservations() {
+    let id = parseInt(sessionStorage.getItem('userid'));
+    this.reservationService
+      .getAllReservationsByRiderID(id)
+      .then((data) => {
+        if (data != null) {
+          this.riderRequest = data;
+          console.log(this.riderRequest);
+          this.color = this.driverCar.color;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  showModal() {
+    console.log(this.Modal + ' Hello From showModal');
+    this.Modal = !this.Modal;
+  }
+  getCarInfo(driverId: string) {
+    let driverIdNumber = parseInt(driverId);
+    this.carService
+      .getCarByUserId(driverIdNumber)
+      .then((data) => {
+        this.driverCar = data;
+
+        this.color = this.CSS_COLOR_NAMES.includes(this.driverCar.color)
+          ? this.driverCar.color
+          : 'orange';
+        console.log('Can you SMELL what the Rock is cooking!');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
