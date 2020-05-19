d57cf74f3de8b269f0b7a7b964b06cad0549b4e6
Brian V <34783078+brianvogelsang@users.noreply.github.com>
2020-05-19 09:18:48 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,18 +8,23 @@ const baseUri = `http://34.238.165.243/`;
  */
 const port = '9002';
 
+const googleKey = '';
+
 /**
  * These are the constants for the production config
  */
 
 export const environment = {
   production: true,
-  environmentName: 'Production Environment',
+  environmentName: 'Default Environment',
   userUri: `${baseUri}:${port}/users/`,
   loginUri: `${baseUri}:${port}/login/`,
   batchesUri: `${baseUri}:${port}/batches/`,
   carUri: `${baseUri}:${port}/cars/`,
   adminUri: `${baseUri}:${port}/admins/`,
   emailUri: `${baseUri}:${port}/email`,
-  reservationDriverUri: `${baseUri}:${port}/reservations/driver`,
+  googleMapKey: `${googleKey}`,
+  reservationUri: `${baseUri}:${port}/reservations`,
+  approveRequestUri: `${baseUri}:${port}/approveDash`,
+  deniedRequestUri: `${baseUri}:${port}/declineDash`,
 };
